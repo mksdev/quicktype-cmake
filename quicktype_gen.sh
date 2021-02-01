deef6011486c5acb81c3b4e32c54e8892e7cd4f5
Michal Němec <michal.nemec.dev@gmail.com>
2021-02-01 01:27:19 +0100

    Cleanup

--- quicktype_gen.sh
+++ quicktype_gen.sh
@@ -1,7 +1,7 @@
 quicktype --no-boost --lang c++ --namespace gen::$3 -s schema "$1" -o "$2"
 
+# fix local include to global include path
 search='#include "json.hpp"'
 replace='#include <json.hpp>'
 
-# Note the double quotes
 sed -i "" "s/${search}/${replace}/g" "$2"
